6b8369500a7ee3b9648cd51105b58bf5cb5b3bfa
[gayathribuddhika] <[wg.gayathri@gmail.com]>
2019-03-03 19:31:35 +0530

    Output HTML
    Output HTML uding echo print in PHP

---
+++ Output HTML.php
@@ -0,0 +1,20 @@
+<input type = "text" name = "name">
+<?php 
+echo "";
+
+?>
+<br>
+<br>
+<?php 
+echo "<input type = 'text' name = 'name'>";
+?>
+<br>
+<br>
+<?php 
+echo '<input type = "text" name = "name">';
+?>
+<br>
+<br>
+<?php 
+echo "<input type = \"text\" name = \"name\" value = \"Gayathri\">";
+?>
